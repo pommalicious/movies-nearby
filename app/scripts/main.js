111324ee190ff13baaa787f18c418dca2cf1512c
pommalicious <willyh@outlook.com>
2012-12-02 23:50:07 -0800

    Basic scaffolding/boilerplates in place

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,33 @@
+// RequireJS Configuration
+var config = {
+    // main.js
+    deps: ["main"],
 
-window.moviesnearby = {
-  Models: {},
-  Collections: {},
-  Views: {},
-  Routers: {},
-  init: function() {
-    console.log('Hello from Backbone!');
-  }
+    // Define paths to common folders/libs
+    paths: {
+        /*
+        transit: "libs/jquery.transit"
+        */
+    },
+
+    // Use this for 3rd party libs that aren't AMD (requirejs) compatible
+    shim: {
+    }
 };
 
-$(document).ready(function(){
-  moviesnearby.init();
+require.config(config);
+
+// Main app entry point: we start here
+require(['Movies', 'router'], function(Movies, Router) {
+
+    Movies.initialize();
+    Movies.router = new Router();
+
+    Backbone.history.start({
+        root: Movies.root
+    });
+
+    console.log("Main started");
+
 });
+
